74dd8cfe3df74cd84986c33839118e37f75a8041
Tongqi Wang <mark3wtq@gmail.com>
2019-04-02 19:32:24 -0500

    Flying
    you can fly now

---
+++ Assets/SAIC-VRclass/Fly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public Transform head;
+
+    public SteamVR_TrackedObject leftHand;
+    public SteamVR_TrackedObject rightHand;
+
+    private bool isFlying = false;
+   
+    void Update()
+    {
+        var lDevice = SteamVR_Controller.Input((int)leftHand.index);
+        var rDevice = SteamVR_Controller.Input((int)rightHand.index);
+        if (lDevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) || rDevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            isFlying = !isFlying;
+        }
+
+        if (isFlying)
+        {
+            Vector3 leftDir = leftHand.transform.position - head.position;
+            Vector3 rightDir = rightHand.transform.position - head.position;
+
+            Vector3 dir = leftDir + rightDir;
+
+            transform.position += dir;
+        }
+    }
+
+}
